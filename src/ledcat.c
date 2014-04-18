4f6f73ac51a30a039d8ef74e36385a33ab3abee9
daniel <info-niftyled@niftylight.de>
2014-04-18 19:50:48 +0200

    adapted to new libniftyled API

--- src/ledcat.c
+++ src/ledcat.c
@@ -408,7 +408,7 @@ int main(int argc, char *argv[])
 
         /* parse prefs-file */
         LedPrefsNode *pnode;
-        if(!(pnode = led_prefs_node_from_file(_c.prefsfile)))
+        if(!(pnode = led_prefs_node_from_file(p, _c.prefsfile)))
         {
                 NFT_LOG(L_ERROR, "Failed to open configfile \"%s\"",
                         _c.prefsfile);
